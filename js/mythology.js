03504d2f4f2ac9fc530de837712a2adafbbf56b6
Soniak85 <genlyshop11@gmail.com>
2026-03-24 22:45:37 +0100

    Create mythology.js file for Norse mythology system

---
+++ js/mythology.js
@@ -0,0 +1,43 @@
+// mythology.js
+
+const NorseGodsAndCreatures = {
+    gods: [
+        {
+            name: "Odin",
+            realm: "Asgard",
+            domain: "Wisdom, War, Death",
+            description: "The Allfather, ruler of the Aesir."
+        },
+        {
+            name: "Thor",
+            realm: "Asgard",
+            domain: "Thunder, Battle, Strength",
+            description: "God of Thunder and protector of mankind."
+        },
+        {
+            name: "Loki",
+            realm: "Jotunheim",
+            domain: "Trickery, Mischief",
+            description: "The trickster god who can change his form."
+        }
+    ],
+    creatures: [
+        {
+            name: "Fenrir",
+            type: "Wolf",
+            description: "A gigantic wolf foretold to kill Odin during Ragnarök."
+        },
+        {
+            name: "Jörmungandr",
+            type: "Serpent",
+            description: "The Midgard Serpent that encircles the Earth."
+        },
+        {
+            name: "Valkyrie",
+            type: "Spirit",
+            description: "Choosers of the slain who bring the souls of warriors to Valhalla."
+        }
+    ]
+};
+
+module.exports = NorseGodsAndCreatures;
